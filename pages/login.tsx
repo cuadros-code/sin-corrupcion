b8d71a7dfdc2ded05db44a82b8be4023c7a62be6
Kevin Cuadros <kevindavidcuadros@gmail.com>
2021-11-26 23:49:14 -0500

    creando componente de login

---
+++ pages/login.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from '../styles/auth.module.css'
+
+const login = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.content_image}>
+
+				</div>
+				<div className={styles.content_form}>
+
+					<form className={styles.form}>
+
+					</form>
+
+				</div>
+        
+    </div>
+  )
+}
+
+export default login
